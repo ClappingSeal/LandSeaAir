d205465a57a57fce791ee224fcf73317d74ca5e8
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-09-19 16:46:04 +0900

    Update Receive.py

--- Receive.py
+++ Receive.py
@@ -6,7 +6,7 @@ while True:
     msg = master.recv_match(blocking=True)
 
     if msg.get_type() == 'DATA64':
-        # 패킹된 데이터 처리 과정
+        # 패딩된 데이터 처리 과정
         received_data = msg.data
         numbers = [int.from_bytes(received_data[i:i + 4], 'little') for i in range(0, len(received_data), 4)]
         numbers = [num for num in numbers if num != 0]
