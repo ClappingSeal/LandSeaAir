cae16d69e95a0ffb449d63c4f6268a875b13b9b5
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-02 21:56:47 +0900

    Update main_computer.py

--- main_computer.py
+++ main_computer.py
@@ -5,6 +5,7 @@ import logging
 
 logging.getLogger('dronekit').setLevel(logging.CRITICAL)
 
+
 class DroneReceiver:
     def __init__(self, connection_string='COM5', baudrate=57600):
         print('vehicle connecting')
@@ -12,19 +13,27 @@ class DroneReceiver:
         self.baudrate = baudrate
         self.vehicle = connect(self.connection_string, wait_ready=True, baud=self.baudrate)
         self.vehicle.add_message_listener('DATA64', self.on_data64)
+        self.received_data = None
 
     def on_data64(self, vehicle, name, message):
         if isinstance(message, ardupilotmega.MAVLink_data64_message):
             data = [int.from_bytes(message.data[i:i + 4], 'little') for i in range(0, len(message.data), 4)]
-            print("Received data:", data)
+            self.received_data = data
+
+    def get_received_data(self):
+        return self.received_data
 
     def close_connection(self):
         self.vehicle.close()
+
 
 if __name__ == "__main__":
     drone_receiver = DroneReceiver()
     try:
         while True:
-            time.sleep(1)  # Keep the script running to continuously listen for incoming messages
+            if drone_receiver.get_received_data():
+                a = drone_receiver.get_received_data()
+                print(a, a[0])
+            time.sleep(1)
     except KeyboardInterrupt:
         drone_receiver.close_connection()
