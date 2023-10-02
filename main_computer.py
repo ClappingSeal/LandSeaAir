8757dda4b4faf296091770b3df40d88fd709773a
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-02 21:32:11 +0900

    Update main_computer.py

--- main_computer.py
+++ main_computer.py
@@ -1,16 +1,30 @@
-from pymavlink import mavutil
+from dronekit import Command, VehicleMode, connect
+from pymavlink.dialects.v20 import ardupilotmega
+import time
+import logging
 
-master = mavutil.mavlink_connection('COM4', baud=57600)
+logging.getLogger('dronekit').setLevel(logging.CRITICAL)
 
-while True:
-    msg = master.recv_match(blocking=True)
+class DroneReceiver:
+    def __init__(self, connection_string='COM5', baudrate=57600):
+        print('vehicle connecting')
+        self.connection_string = connection_string
+        self.baudrate = baudrate
+        self.vehicle = connect(self.connection_string, wait_ready=True, baud=self.baudrate)
+        self.vehicle.add_message_listener('DATA64', self.on_data64)
 
-    if msg.get_type() == 'DATA64':
-        # 패킹된 데이터 처리 과정
-        received_data = msg.data
-        numbers = [int.from_bytes(received_data[i:i + 4], 'little') for i in range(0, len(received_data), 4)]
-        numbers = [num for num in numbers if num != 0]
-        print("Received data:", numbers)
+    def on_data64(self, vehicle, name, message):
+        if isinstance(message, ardupilotmega.MAVLink_data64_message):
+            data = [int.from_bytes(message.data[i:i + 4], 'little') for i in range(0, len(message.data), 4)]
+            print("Received data:", data)
 
-# 이 코드에서 종료는 실행되지 않음
-master.close()
+    def close_connection(self):
+        self.vehicle.close()
+
+if __name__ == "__main__":
+    drone_receiver = DroneReceiver()
+    try:
+        while True:
+            time.sleep(1)  # Keep the script running to continuously listen for incoming messages
+    except KeyboardInterrupt:
+        drone_receiver.close_connection()
