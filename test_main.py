078fecfc4c7be058433a7e7ccd16507c451b3de1
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-27 20:45:50 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -265,7 +265,10 @@ if __name__ == '__main__':
             x_conversion = x - 425
             y_conversion = y - 240
             r = (abs(x_conversion) + abs(y_conversion)) / 10
-            theta = math.atan(x_conversion + 0.1 / y_conversion + 1) * -60
+            
+            print(r, x_conversion, y_conversion)
+            theta = math.atan(x_conversion + 0.1 / y_conversion + 1)
+            print(theta)
             if y_conversion < 0:
                 r = -r
             return theta, r
