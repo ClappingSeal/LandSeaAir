3542a535b3062eb47d268098ff228f5c0fda9f9e
junkishim <105273906+junkishim@users.noreply.github.com>
2023-10-28 00:39:40 +0900

    Update test_main2.py

--- test_main2.py
+++ test_main2.py
@@ -12,7 +12,7 @@ logging.getLogger('dronekit').setLevel(logging.CRITICAL)
 
 
 class Drone:
-    def __init__(self, connection_string='/dev/ttyACM0', baudrate=115200, udp_ip="192.168.144.25", udp_port=37260):
+    def __init__(self, connection_string='/dev/ttyACM0', baudrate=115200, udp_ip="192.168.144.26", udp_port=37260):
 
         # Connecting value
         self.connection_string = connection_string
@@ -89,11 +89,6 @@ class Drone:
         if not self.camera.isOpened():
             print("Error: Couldn't open the camera.")
             return
-
-        if self.serial_port.isOpen():
-            print("Connection is established!")
-        else:
-            print("Error in serial connection!")
 
     # color camera test1
     def detect_and_find_center(self, x=1.3275, save_image=True, image_name="captured_image.jpg"):
