8b823bb59265c57994ef6c6eaf46341e7a6b4a3e
junkishim <105273906+junkishim@users.noreply.github.com>
2023-10-28 19:27:02 +0900

    Update test_main2.py

--- test_main2.py
+++ test_main2.py
@@ -298,6 +298,12 @@ if __name__ == '__main__':
 
 
         try:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.bind(('0.0.0.0', 12345))
+            server_socket.listen(1)
+
+            print("Waiting for connection...")
+            
             while True:
                 step += 1
                 sending_array = drone.detect_and_find_center()
@@ -318,17 +324,13 @@ if __name__ == '__main__':
 
                     drone.set_gimbal_angle(yaw, pitch)
 
-                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_socket.bind(('0.0.0.0', 12345))
-                server_socket.listen(1)
 
-                print("Waiting for connection...")
                 client_socket, addr = server_socket.accept()
 
                 data_to_send = "Data from Raspberry Pi"
                 client_socket.sendall(data_to_send.encode())
                 client_socket.close()
-                
+
         except KeyboardInterrupt:
             drone.images_to_avi("captured_image", "output.avi")
             print("Video saved as output.avi")
