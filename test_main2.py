b67a967a76f1bec3f8344071bdb7496d9ff9033d
junkishim <105273906+junkishim@users.noreply.github.com>
2023-10-28 13:28:06 +0900

    Update test_main2.py

--- test_main2.py
+++ test_main2.py
@@ -321,7 +321,7 @@ if __name__ == '__main__':
                 sending_data = [sending_array[0], sending_array[1], truth]
 
                 drone.adjust_gimbal_relative_to_current(sending_array[0], sending_array[1])
-                time.sleep(1)
+                time.sleep(0.1)
                 # drone.sending_data(sending_data)
                 # print(sending_data)
                 # # print(drone.receiving_data())
