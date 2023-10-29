d874ed37b0547b20b5a260d2ff8bd44350152958
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-30 00:09:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ if __name__ == '__main__':
         step = 0
         drone.set_gimbal_angle(yaw, pitch)
         time.sleep(1.5)
-        # drone.set_gimbal_angle(0, -45)
+        # drone.set_gimbal_angle(0, -80)
         # time.sleep(1.5)
 
         try:
