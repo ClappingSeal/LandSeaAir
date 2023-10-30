4619b91fc37f3bac88f155a59200e5b39b94fd85
junkishim <105273906+junkishim@users.noreply.github.com>
2023-10-30 19:57:49 +0900

    modified:   test_main_computer2.py

--- test_main_computer2.py
+++ test_main_computer2.py
@@ -333,7 +333,7 @@ if __name__ == "__main__":
                 # receive_arr = np.array(gt.receiving_data())
                 # print(receive_arr)
                 # gt.locking_easy(receive_arr[0], receive_arr[1], 300) # 마지막 숫자가 줄어들면 빨라짐
-                if len(data_list[0]) == 3 & data_list[2] == 1:
+                if len(str(data_list[0])) == 3 and data_list[2] == 1:
                     gt.locking_easy(data_list[0], data_list[1], 300) # 마지막 숫자가 줄어들면 빨라짐
                 gt.update_past_pos_data()
                 time.sleep(0.1)
