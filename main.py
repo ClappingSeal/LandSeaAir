d48a2700b0f47265878d84ace3d7b56c0e586964
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-31 18:34:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ class Drone:
         self.label = None
         self.labels = ['hybrid', 'fixed', 'quadcopter']
         self.previous_centers = []
-        self.center_count = 3
+        self.center_count = 2
         self.tolerance = 100
         self.tracker_initialized = False
         self.tracker = None
@@ -392,7 +392,7 @@ if __name__ == '__main__':
                 if sending_array == None:
                     sending_array = [drone.frame_width_divide_2, drone.frame_height_divide_2, 0]
                 truth = 0
-                if sending_array[1] != 240:
+                if sending_array[1] != drone.frame_width_divide_2:
                     truth = 1
                 sending_data = [sending_array[0], sending_array[1], drone.truth]
 
@@ -415,4 +415,3 @@ if __name__ == '__main__':
             drone.images_to_avi("captured_image", "output.avi")
             print("Video saved as output.avi")
             drone.close_connection()
-
