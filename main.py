9d5d75b4bd20fd22f6d161438295791cee095c72
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-31 16:48:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -109,49 +109,12 @@ class Drone:
         self.frame_count = 0
         self.recheck_interval = 15  # 드론 재확인 간격
 
-    # color camera test1
-    def detect_and_find_center(self, x=1.3275, save_image=True):
-        ret, frame = self.camera.read()
-        if not ret:
-            print("Error: Couldn't read frame.")
-            return (425, 240)
-
-        # Resize frame considering the aspect ratio multiplier
-        h, w = frame.shape[:2]
-        res_frame = cv2.resize(frame, (int(w * x), h))
-
-        hsv = cv2.cvtColor(res_frame, cv2.COLOR_BGR2HSV)
-
-        lower_bound = np.array([self.base_color[0] - self.threshold, 130, 130])
-        upper_bound = np.array([self.base_color[0] + self.threshold, 255, 255])
-
-        mask = cv2.inRange(hsv, lower_bound, upper_bound)
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        center = (425, 240)
-        if contours:
-            largest_contour = max(contours, key=cv2.contourArea)
-            M = cv2.moments(largest_contour)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                center = (cX, cY)
-                print('sadf')
-
-        # Always draw the circle at the detected center (or default if no center detected)
-        cv2.circle(res_frame, center, 10, (100, 100, 100), -1)
-
-        if save_image:
-            self.image_count += 1
-            image_name = f"captured_image_{self.image_count}.jpg"
-            cv2.imwrite(image_name, res_frame)
-
-        return (center[0], 480 - center[1])
+        self.video_writer = cv2.VideoWriter('output_video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30.0, (850, 480))
 
     # drone camera 1 (drone detection return [x, y, label] None if not detected)
     def __del__(self):
         self.camera.release()
+        self.video_writer.release()  # 비디오 라이터 리소스 해제
 
     # drone camera 2
     def detect(self):
@@ -171,9 +134,10 @@ class Drone:
                 if self.frame_count % self.recheck_interval == 0:
                     if not self.is_drone(frame_resized, bbox):
                         self.tracker_initialized = False  # 드론이 아니라면 트래커 초기화
-                # 추적하는 동안 이미지 저장
-                cv2.imwrite(f"captured_image_{self.capture_count}.jpg", frame_resized)
-                self.capture_count += 1
+
+                # 프레임 녹화
+                self.video_writer.write(frame_resized)
+
                 return x + w // 2, 480 - (y + h // 2), w, h  # 중심 좌표 반환
             else:
                 self.tracker_initialized = False  # 추적 실패 시 초기화
@@ -191,8 +155,6 @@ class Drone:
             center_x, center_y, width, height = self.get_center_and_dimensions(best_data)
             cv2.rectangle(frame_resized, (center_x - width // 2, center_y - height // 2),
                           (center_x + width // 2, center_y + height // 2), (0, 255, 0), 2)
-            cv2.imwrite(f"captured_image_{self.capture_count}.jpg", frame_resized)
-            self.capture_count += 1
 
             self.previous_centers.append((center_x, center_y))
             if len(self.previous_centers) > self.center_count:
@@ -204,8 +166,8 @@ class Drone:
                 self.tracker.init(frame_resized, bbox)
                 self.tracker_initialized = True
 
-        cv2.imwrite(f"captured_image_{self.capture_count}.jpg", frame_resized)
-        self.capture_count += 1
+        # 프레임 녹화
+        self.video_writer.write(frame_resized)
 
         if best_data and best_confidence > self.confidence_threshold:
             return center_x, 480 - center_y, width, height
@@ -328,26 +290,26 @@ class Drone:
     # gimbal 5
     def accquire_data(self):
         self.send_command_to_gimbal(b'\x55\x66\x01\x00\x00\x00\x00\x0d\xe8\x05')
-        
+
         try:
-            response, addr = self.udp_socket.recvfrom(1024) 
+            response, addr = self.udp_socket.recvfrom(1024)
             # print("Received:", response)
             return response
         except socket.error as e:
             print(f"Error receiving data via UDP: {e}")
             return None
-    
+
     # gimbal 6
-    def acquire_attitude(self,response):
+    def acquire_attitude(self, response):
         # CMD ID를 찾습니다.
         index_0d = response.find(b'\x0D')
 
         # Yaw, Pitch, Roll 데이터를 추출합니다.
-        data_06 = response[index_0d+1:index_0d+7]
+        data_06 = response[index_0d + 1:index_0d + 7]
         yaw_raw, pitch_raw, roll_raw = struct.unpack('<hhh', data_06)
 
         # Yaw Velocity, Pitch Velocity, Roll Velocity 데이터를 추출합니다.
-        data_0c = response[index_0d+7:index_0d+15]
+        data_0c = response[index_0d + 7:index_0d + 15]
         yaw_velocity_raw, pitch_velocity_raw, roll_velocity_raw, _ = struct.unpack('<hhhh', data_0c)
 
         # 추출한 데이터를 10으로 나눠 실제 값으로 변환합니다.
@@ -359,11 +321,11 @@ class Drone:
         roll_velocity = roll_velocity_raw / 10.0
 
         return yaw, pitch, roll, yaw_velocity, pitch_velocity, roll_velocity
-    
+
     def capture_image(self, num1, num2, num3, num4, num5):
         for _ in range(5):  # 더미 이미지 5장 캡처
             self.camera.read()
-            
+
         ret, frame = self.camera.read()
 
         if ret:
@@ -421,31 +383,26 @@ if __name__ == '__main__':
 
     start_command = input("Press 's' to start: ")
 
-    if start_command == 's':
-        
+    if start_command.lower() == 's':  # 대소문자 구분 없이 's'를 입력받기 위해 .lower() 메소드 사용
+
         drone = Drone()
 
-        yaw = 0
-        pitch = 0
-        drone.set_gimbal_angle(yaw, pitch)
-        yaw = 0
-        pitch = -90
-        drone.set_gimbal_angle(yaw, pitch)
-        time.sleep(1.5)
+        # 초기 감블 각도 설정을 함수로 추출하여 코드 중복 제거
+        def set_initial_gimbal_angle(yaw, pitch, wait=1.5):
+            drone.set_gimbal_angle(yaw, pitch)
+            time.sleep(wait)
+
+        set_initial_gimbal_angle(0, 0)
+        set_initial_gimbal_angle(0, -90)
 
         try:
             while True:
-                sending_array = drone.detect()
-        
-                 # reformatting data
-                if sending_array == None:
-                    sending_array = [425, 240, 0]
-                truth = 0
-                if sending_array[1] != 240:
-                    truth = 1
+                sending_array = drone.detect() or [425, 240, 0]  # None이면 기본값 설정
+
+                truth = int(sending_array[1] != 240)  # truth 값 계산을 보다 명확하게
                 sending_data = [sending_array[0], sending_array[1], truth]
-        
-                # sending data
+
+                # 데이터 전송
                 drone.sending_data(sending_data)
                 print(sending_data)
                 time.sleep(0.1)
