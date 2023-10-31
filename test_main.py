e5cde50465d93e04cb1ac5da1fae2cb73d0549de
junkishim <105273906+junkishim@users.noreply.github.com>
2023-10-31 14:19:24 +0900

    modified:   test_main.py

--- test_main.py
+++ test_main.py
@@ -434,9 +434,9 @@ if __name__ == '__main__':
                         print("Pitch:", pitch_wr)
                         print("Roll:", roll_wr)
                         break
-                time.sleep(0.2)
+                time.sleep(2)
                 drone.capture_image(yaw, pitch, yaw_wr, pitch_wr, roll_wr)
-                time.sleep(0.2)
+                time.sleep(2)
 
         try:
             while True:
