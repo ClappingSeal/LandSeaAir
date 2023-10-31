5b0705d8715f1f290359c9633288b3176c3022f4
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-10-31 20:03:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -159,6 +159,16 @@ class Drone:
         ret, frame = self.camera.read()
         if not ret:
             return self.frame_width_divide_2, self.frame_height_divide_2, 0, 0
+    
+        # 이미지 전처리 시작: 필터 적용
+        frame = cv2.GaussianBlur(frame, (3, 3), 1)
+        frame = cv2.bilateralFilter(frame, 9, 80, 80)
+        kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        frame = cv2.filter2D(frame, -1, kernel)
+        frame = cv2.medianBlur(frame, 5)
+        alpha = 0.9
+        frame = cv2.addWeighted(frame, alpha, np.zeros(frame.shape, frame.dtype), 0, 0)
+        # 이미지 전처리 끝
 
         frame_resized = cv2.resize(frame, None, fx=self.scale_factor, fy=1)
         self.frame_count += 1
