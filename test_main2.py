90629f46b3b09e0ee320d044d0fba15a5bd79e3e
junkishim <105273906+junkishim@users.noreply.github.com>
2023-11-01 13:46:40 +0900

    modified:   test_main2.py

--- test_main2.py
+++ test_main2.py
@@ -357,6 +357,24 @@ class Drone:
             print("Error: {}".format(e))
             return 10000, 10000, 10000, 10000, 10000, 10000
 
+    # <계산식> 현재 각도 (yaw, pitch)에 대해 기준 (0, -90) 에 맞게 frame 값 출력
+    def angle_cali(x, y, yaw, pitch): # 기준 yaw = 0, pitch = -90 ### pitch = -60을 기준으로 하려면 아래 숫자 90 -> 60 수정해야 함.
+        pivot_x=425
+        pivot_y=240
+
+        yaw_rad = math.radians(yaw)
+
+        translated_x = x - pivot_x
+        translated_y = y - pivot_y
+
+        rotated_x = translated_x * math.cos(yaw_rad) - translated_y * math.sin(yaw_rad)
+        rotated_y = translated_x * math.sin(yaw_rad) + translated_y * math.cos(yaw_rad)
+
+        x_new = rotated_x + pivot_x + ((pitch + 90) * (130/15)) # 15도당 130프레임
+        y_new = rotated_y + pivot_y
+
+        return x_new, y_new
+
     def close_connection(self):
         self.vehicle.close()
 
