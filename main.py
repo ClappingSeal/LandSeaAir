405bea20007cf0fe10afb4aa8e2401245a298dab
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-01 13:38:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class Drone:
 
         # detection requirements
         self.model = YOLO('Tech_piece/Detection/best3.onnx')
-        self.confidence_threshold = 0.1
+        self.confidence_threshold = 0.3
         self.scale_factor = 1.3275
         self.capture_count = 0
         self.label = None
