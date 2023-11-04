52b4c1e771e96efafab9642990a5a2fc4729f360
junkishim <105273906+junkishim@users.noreply.github.com>
2023-11-04 16:22:40 +0900

    modified:   exploration_main_computer.py

--- exploration_main_computer.py
+++ exploration_main_computer.py
@@ -413,7 +413,7 @@ if __name__ == "__main__":
                 time.sleep(0.1)
                 cnt = data_list[4]
                 print("cnt:", cnt)
-                
+
                 ### 드론 기동
                 if cnt < 100: # 두리번두리번
                     _, target_y = gt.get_pos()
@@ -427,10 +427,23 @@ if __name__ == "__main__":
                             yaw_set += 2 * direction
                         yaw_set += direction
                         print('set', yaw_set)
+                    time.sleep(0.1)
+
                 elif cnt > 100: # 서쪽 고정을 위한 기동 및 서쪽
                     print('=========================')
-                    print('=========================')
-                    print('=========================')
+                    target_x, target_y = gt.get_pos()
+                    if data_list[0] > 625: # 드론 오른쪽에 있을 때
+                        gt.velocity_pid(target_x+5, target_y+5, velocity_z=0, history_positions=gt.past_pos_data)
+                    elif data_list[1] < 225: # 드론 왼쪽에 있을 때
+                        gt.velocity_pid(target_x-5, target_y+5, velocity_z=0, history_positions=gt.past_pos_data)
+                    else: # locking
+                        gt.set_yaw_to_west_nonblock()
+                        if len(str(data_list[0])) == 3 and data_list[2] == 1:
+                            # gt.locking_easy(data_list[0], data_list[1], 300) # 마지막 숫자가 줄어들면 빨라짐
+                            gt.locking_drl(data_list[0], data_list[1],data_list[3])
+                        gt.update_past_pos_data()
+                    time.sleep(0.1)
+                        
 
 
                     
