332876c21c679f78fc1611e2b62ca4779f775771
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-20 19:53:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ class Drone:
 
         # gimbal initial angle
         self.init_yaw = 0
-        self.init_pitch = 70
+        self.init_pitch = 15
 
     def detect1(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
