1b709b53bad08ed60281db82da4bb62c30f84ad9
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-20 16:19:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ class Drone:
 
         # gimbal initial angle
         self.init_yaw = 0
-        self.init_pitch = 40
+        self.init_pitch = 30
 
     def detect1(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -413,10 +413,10 @@ if __name__ == '__main__':
                     # drone.init_yaw = 59
                 # if drone.init_yaw < -59:
                     # drone.init_yaw = -59
-                if drone.init_pitch > 90:
-                    drone.init_pitch = 90
-                if drone.init_pitch < 60:
-                    drone.init_pitch = 60
+                if drone.init_pitch > 80:
+                    drone.init_pitch = 80
+                if drone.init_pitch < 30
+                    drone.init_pitch = 30
 
                 drone.set_gimbal_angle_feedback(drone.init_yaw, drone.init_pitch)
 
